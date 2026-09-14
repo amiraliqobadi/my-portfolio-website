cbebd6f31166373ddd15242189a58e98148d31f3
amiraliqobadi <amiraliqobadi5@gmail.com>
2026-09-14 14:04:16 +0330

    feat: launch interactive driving portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import { LanguageProvider } from "./i18n/LanguageContext";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import TechStack from "./components/TechStack";
-import Achievements from "./components/Achievements";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import InteractivePortfolio from "./components/InteractivePortfolio";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 function App() {
     return (
         <LanguageProvider>
-            <div className="bg-black min-h-screen">
-                <Navbar />
-                <Hero />
-                <About />
-                <Projects />
-                <TechStack />
-                <Achievements />
-                <Contact />
-                <Footer />
-                <SpeedInsights />
-            </div>
+            <InteractivePortfolio />
+            <SpeedInsights />
         </LanguageProvider>
     );
 }
